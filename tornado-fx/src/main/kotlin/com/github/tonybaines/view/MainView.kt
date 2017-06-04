57890cb7211a16368fe0b013c4b2bb45983b7b6f
Tony Baines <tony.baines.1@gmail.com>
2017-06-04 18:56:16 +0100

    Updating graph

--- tornado-fx/src/main/kotlin/com/github/tonybaines/view/MainView.kt
+++ tornado-fx/src/main/kotlin/com/github/tonybaines/view/MainView.kt
@@ -1,12 +1,33 @@
 package com.github.tonybaines.view
 
 import com.github.tonybaines.app.Styles
+import javafx.collections.FXCollections
+import javafx.collections.ObservableList
+import javafx.scene.chart.CategoryAxis
+import javafx.scene.chart.NumberAxis
+import javafx.scene.chart.XYChart
 import tornadofx.*
+import java.time.Instant
 
 class MainView : View("Hello TornadoFX Application") {
-    override val root = hbox {
-        label(title) {
-            addClass(Styles.heading)
+    val myData: ObservableList<XYChart.Data<String, Number>> = FXCollections.observableArrayList()
+    var value = 1
+
+    override val root = vbox {
+        hbox {
+            label(title) {
+                addClass(Styles.heading)
+            }
+            button(text = "Click me") {
+                setOnMouseClicked {
+                    myData.add(XYChart.Data(Instant.now().toString(), value ++))
+                }
+            }
+        }
+        linechart(title = "Line Chart", x = CategoryAxis(), y = NumberAxis()) {
+            series("Time") {
+                data = myData
+            }
         }
     }
 }
